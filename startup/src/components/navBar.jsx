3a1c2ffb18f9081db5bd9c429c6bdf9c1e3a93a2
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-10-24 12:07:11 -0500

    Basic startup example code

---
+++ startup/src/components/navBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NavigationBar() {
+  return (
+    <div className="NavigationBar">
+      NavBar
+    </div>
+  );
+}
+
+export default NavigationBar;
--- startup/src/components/template.jsx
+++ startup/src/components/template.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import NavigationBar from './components/navBar';
 
 function App() {
   return (
     <div className="App">
-      <NavigationBar />
+      
     </div>
   );
 }
